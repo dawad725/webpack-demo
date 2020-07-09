55a2b3be9cfc2568b352a6f35314cc7b5842dac4
daveman235 <awad.david@gmail.com>
2020-07-09 17:22:44 -0400

    added sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,11 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+                test: /\.scss$/,
+                use: [
+                    "style-loader",  //3. Inject styles into dom
+                    "css-loader",  //2. Turns css in common.js 
+                    "sass-loader"] //1. Turns sass into css 
             }
         ]
     }
