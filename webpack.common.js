7d4c583bcfdd21cf529fa20f81d5ac0919701f8b
daveman235 <awad.david@gmail.com>
2020-07-10 13:48:51 -0400

    Minify HTML CSS and JS in production

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,14 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
     entry: {
         main: "./src/index.js",
         vendor: "./src/vendor.js"
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html"
-        })
-    ],
     module: {
         rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader",  //3. Inject styles into dom
-                    "css-loader",    //2. Turns css in common.js 
-                    "sass-loader"    //1. Turns sass into css 
-                ]
-            },
             {
                 test: /\.html$/,
                 use: ["html-loader"]
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,29 @@
 const common = require("./webpack.common");
 const { merge } = require('webpack-merge');
 const path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
     mode: "development",
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",  //3. Inject styles into dom
+                    "css-loader",    //2. Turns css in common.js 
+                    "sass-loader"    //1. Turns sass into css 
+                ]
+            },
+        ]
     }
 }); 
