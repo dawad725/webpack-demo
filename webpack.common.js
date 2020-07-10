848802024c97423260eb6c4741a5b5a096e2c030
daveman235 <awad.david@gmail.com>
2020-07-10 11:50:31 -0400

    added html-loader and file loader to deal with images and added clean-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -14,9 +14,25 @@ module.exports = {
                 test: /\.scss$/,
                 use: [
                     "style-loader",  //3. Inject styles into dom
-                    "css-loader",  //2. Turns css in common.js 
-                    "sass-loader"] //1. Turns sass into css 
+                    "css-loader",    //2. Turns css in common.js 
+                    "sass-loader"    //1. Turns sass into css 
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outputPath: "imgs"
+                    }
+                }
             }
         ]
+
     }
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 const { merge } = require('webpack-merge');
 const common = require("./webpack.common");
-
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 
 module.exports = merge(common, {
@@ -10,5 +10,6 @@ module.exports = merge(common, {
     output: {
         filename: "main.[contentHash].js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    plugins: [new CleanWebpackPlugin()]
 });
